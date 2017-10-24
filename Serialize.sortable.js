628de8e4cf041e3a37a5f12e5dfc0a2d25d2c448
Wendel <gswendelw3ss@gmail.com>
2017-10-24 12:45:31 -0200

    Update Serialize.sortable.js

--- Serialize.sortable.js
+++ Serialize.sortable.js
@@ -20,3 +20,48 @@ $(function () {
         console.log(r, a);
     });
 });
+
+
+//
+So here's how I would serialize this, into an object with two members, src_arr and caption_arr:
+
+var getPaths = function() {
+    var imgPaths = { 'src_arr': [], 'caption_arr': []};
+    $('.gallery img').each(function(){
+        imgPaths.src_arr.push($(this).attr('src'));
+        imgPaths.caption_arr.push($(this).attr('alt'));
+    });
+    return imgPaths;
+};
+So I'd do this with your code:
+
+$.ajax({
+    url: "sort.php",
+    type: "POST",
+    dataType: 'html',
+    data: getPaths(),
+    success: function(data, textStatus, XMLHttpRequest) {
+        // you need to do something in here
+        $('#debug').html('<pre>' + data + '</pre>');
+    },
+    error: function() {
+        alert("theres an error with AJAX");
+    }
+});
+The raw data as print_r() out of sort.php looks like:
+
+Array
+(
+    [src] => Array
+        (
+            [0] => tn/001.jpg
+            [1] => tn/002.jpg
+        )
+
+    [caption] => Array
+        (
+            [0] => first caption
+            [1] => second caption with Ã©Ã¨Ã§Ã  international chars
+        )
+
+)
